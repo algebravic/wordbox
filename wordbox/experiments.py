b1bb485c73fbd44e492237a6ca3a753f61fd933e
Victor S. Miller <victorsmiller@gmail.com>
2023-06-15 14:29:39 -0700

    Update python version.  Add flymake suggestions

--- wordbox/experiments.py
+++ wordbox/experiments.py
@@ -12,33 +12,38 @@ ex = Experiment('Word Box')
 
 @ex.config
 def wordbox_config():
-
+    """ Default configuration for wordbox """
     mval = 5
     nval = 7
     wordlist = 'planets'
-    solver = 'cadical'
+    solver = 'cadical153'
     coloring = True
     bip = True
     cardinality = -1 # No extra cardinality constraints
     time_limit = -1.0 # no limit
 
 @ex.main
-def solveit(mval, nval, wordlist, solver, coloring, bip, cardinality, time_limit):
-
+def solveit(mval, nval, wordlist,
+            solver, coloring, bip,
+            cardinality, time_limit):
+    """ Solve the problem """
     if time_limit < 0:
         return run_solver(mval, nval, wordlist, solver, coloring, bip, cardinality)
-        
-    print("Using time limit {}".format(time_limit))
+
+    print(f"Using time limit {time_limit}")
     with ThreadPoolExecutor(2) as excecutor:
-        future = excecutor.submit(run_solver, mval, nval, wordlist, solver, coloring, bip, cardinality, time_limit=time_limit)
+        future = excecutor.submit(
+            run_solver, mval, nval,
+            wordlist, solver, coloring,
+            bip, cardinality, time_limit=time_limit)
     try:
         return future.result(timeout=time_limit)
-    except TimeoutError:
-        raise TimeoutInterrupt()
+    except TimeoutError as exc:
+        raise TimeoutInterrupt() from exc
 
 def run_main():
+    """ Main command for sacred """
     ex.run_commandline()
 
 if __name__ == '__main__':
     run_main()
-    
